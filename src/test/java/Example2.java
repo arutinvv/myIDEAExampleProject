df72a8af65bf44b451a7fa8a9d30a0b1e6ac9e7f
arutinvv <arutinus@gmail.com>
2021-11-15 21:06:44 +0200

    just a new commit delete

--- src/test/java/Example2.java
+++ src/test/java/Example2.java
@@ -1,10 +1,6 @@
 public class Example2 {
     public static void main(String[] args) {
-String s ="text";
-        String tts ="text";
-        System.out.println("Git");
-        double c =4.5;
-        int number =145;
+
     }
 
 
